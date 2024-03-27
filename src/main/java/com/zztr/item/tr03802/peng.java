2ffe3ac7350d543d5f73678b4dee99207cfa5a86
peng1232 <1439438940@qq.com>
2024-03-27 10:57:56 +0800

    peng

---
+++ src/main/java/com/zztr/item/tr03802/peng.java
@@ -0,0 +1,13 @@
+package com.zztr.item.tr03802;
+
+/**
+* @Description: 
+* @author: 铭
+* @date 2024年3月27日 上午10:57:38
+*/
+
+public class peng {
+	public static void main(String[] args) {
+		System.out.println(111);
+	}
+}
